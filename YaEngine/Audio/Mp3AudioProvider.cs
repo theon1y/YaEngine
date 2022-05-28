58b928f33961265e459896bcb194ca81e195dfb4
Maksim Skrupski <skrupski@reaction.games>
2022-05-28 17:12:48 +0500

    Add mp3 audio provider

---
+++ YaEngine/Audio/Mp3AudioProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MP3Sharp;
+
+namespace YaEngine.Audio
+{
+    public class Mp3AudioProvider : IAudioProvider
+    {
+        private readonly MP3Stream stream;
+
+        public Mp3AudioProvider(string filePath)
+        {
+            stream = new MP3Stream(filePath);
+        }
+
+        public AudioProperties ReadProperties()
+        {
+            return new()
+            {
+                NumChannels = stream.ChannelCount,
+                SampleRate = stream.Frequency,
+                BitsPerSample = 16,
+            };
+        }
+
+        public byte[] GetAudioData()
+        {
+            using var memoryStream = new MemoryStream();
+            stream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+        
+        public void Dispose()
+        {
+            stream.Dispose();
+        }
+    }
+}
